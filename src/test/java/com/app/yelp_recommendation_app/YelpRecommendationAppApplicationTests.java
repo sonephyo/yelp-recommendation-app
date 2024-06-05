1d48ee5230f465ab1a716d584c20536198fb1bc1
sonephyo <sonephyo7777777@gmail.com>
2024-06-04 21:41:20 -0400

    initalizing

---
+++ src/test/java/com/app/yelp_recommendation_app/YelpRecommendationAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.yelp_recommendation_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YelpRecommendationAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

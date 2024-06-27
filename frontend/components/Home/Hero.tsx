b01f71e5876fe10351de2a69a79567d16817d43c
sonephyo <sonephyo7777777@gmail.com>
2024-06-27 00:34:57 -0400

    finishing up what i learn today

--- frontend/components/Home/Hero.tsx
+++ frontend/components/Home/Hero.tsx
@@ -10,28 +10,30 @@ const Hero = () => {
         <Box topPosition={"10vh"} leftPosition={"90vw"} />
       </div>
 
-      <div className=" flex flex-col lg:flex-row">
+      <div className=" flex flex-col lg:flex-row justify-center gap-9">
         <div className="relative h-[70vh] lg:hidden">
           <p className="absolute w-full font-bold text-5xl italic flex flex-row justify-center items-center z-20 bottom-[20vh] ">
             Whereabouts
           </p>
         </div>
-        <div className="w-[80vw] lg:w-[30rem] mx-auto lg:mx-20 flex flex-col gap-2 lg:h-screen justify-center">
-          <h1 className="hidden lg:blockfont-bold text-5xl italic">
+        <div className="w-[80vw] sm:w-[25rem] mx-auto lg:mx-20 lg:-translate-x-32 flex flex-col gap-2 lg:h-screen justify-center">
+          <h1 className="hidden lg:block font-bold text-5xl italic">
             Whereabouts
           </h1>
-          <h3 className="text-2xl font-semibold">Explore Experience Connect</h3>
-          <p>
+          <h3 className="text-3xl font-semibold">Explore Experience Connect</h3>
+          <p className="md:w-[20rem] md:text-lg">
             A system that recommends user stores to explore while keeping
             experience similar to what they connect
           </p>
           <Cbutton text="Try it out" classes="w-[8rem]" />
         </div>
-        <div className=" grid grid-rows-3 grid-flow-col h-full my-auto rotate-45">
+        <div className="hidden lg:grid lg:grid-rows-3 grid-flow-col h-full my-auto rotate-45 gap-2">
           {[1, 2, 3, 4, 5, 6].map((index) => (
             <div
               key={index}
-              className={`${ (index == 1 || index == 6) ? ' invisible' : ''} w-[150px] h-[150px] border-8  border-cBlue bg-cLightBlue rounded-[18px] `}
+              className={`${
+                index == 1 || index == 6 ? " invisible" : ""
+              } w-[150px] h-[150px] border-4  border-cBlue bg-cLightBlue rounded-xl `}
               style={{
                 boxShadow: "14px 0px 45.9px 0px #74B8FF",
               }}

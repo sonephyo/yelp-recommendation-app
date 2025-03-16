bc8fa712c9cc0e7cbdeb49f658187bbb6e808671
Soney <sonephyo7777777@gmail.com>
2025-03-16 13:53:04 -0400

    feat: updating Features section with video demo

--- frontend/components/Home/OfferInfo.tsx
+++ frontend/components/Home/OfferInfo.tsx
@@ -5,8 +5,7 @@ import Image from "next/image";
 
 const OfferInfo = () => {
   const [isOpen, setisOpen] = useState<boolean>(false);
-  const videoRef = useRef<HTMLVideoElement>(null);
-
+  const [selectedVideo, setSelectedVideo] = useState<string>("");
   return (
     <div className="flex flex-col items-center my-10">
       <h2 className=" text-4xl font-bold italic mb-3 md:mb-10 ">Features</h2>
@@ -30,8 +29,7 @@ const OfferInfo = () => {
                 className="btn"
                 onClick={() => {
                   setisOpen(true);
-                  videoRef.current!.currentTime = 0;
-                  videoRef.current!.play();
+                  setSelectedVideo("/videos/selectBusiness.mov");
                 }}
               >
                 See Demo
@@ -50,16 +48,29 @@ const OfferInfo = () => {
           </figure>
           <div className="card-body">
             <p className=" text-lg leading-5 font-semibold">
-                Dynamic Searching for you to research your store
+              Dynamic Searching for you to research your store
             </p>
             <p></p>
             <div className="card-actions justify-center">
-              <button className="btn">See Demo</button>
+              <button
+                className="btn"
+                onClick={() => {
+                  setisOpen(true);
+                  setSelectedVideo("/videos/dynamicsearch.mov");
+                }}
+              >
+                See Demo
+              </button>
             </div>
           </div>
         </div>
       </div>
-      <PopUpBox isOpen={isOpen} setisOpen={setisOpen} videoRef={videoRef} />
+      <PopUpBox
+        isOpen={isOpen}
+        setisOpen={setisOpen}
+        videoSrc={selectedVideo}
+        setSelectedVideo={setSelectedVideo}
+      />
     </div>
   );
 };
@@ -67,35 +78,43 @@ const OfferInfo = () => {
 const PopUpBox = ({
   isOpen,
   setisOpen,
-  videoRef,
+  videoSrc,
+  setSelectedVideo
 }: {
   isOpen: boolean;
   setisOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  videoRef: React.RefObject<HTMLVideoElement>;
+  videoSrc: string;
+  setSelectedVideo: React.Dispatch<React.SetStateAction<string>>
 }) => {
   return (
     <div className={`${isOpen ? "block" : "hidden"}`}>
       <button
         className="w-10 fixed z-[60] top-[calc(50vh-31vh)] lg:top-[10vh] right-[10vw] bg-white p-3 rounded-full shadow-xl shadow-cButtonShadowBlue landscape:-translate-y-10 "
         onClick={() => {
-          setisOpen(false);
+          setisOpen(false)
+          setSelectedVideo("")
         }}
       >
         <Image src="/logo/cross_nav.svg" alt="Close" width={500} height={500} />
       </button>
-      <video
-        controls
-        autoPlay
-        muted
-        loop
-        ref={videoRef}
-        className="fixed bg-slate-900 w-[80vw] h-[50vh] lg:h-[70vh] z-[60] top-[calc(50vh-25vh)] lg:top-[15vh] left-[10vw] rounded-[3rem] shadow-xl shadow-cButtonShadowBlue"
-      >
-        <source src={"/video.mov"} type="video/mp4" />
-      </video>
+      {videoSrc && (
+        <video
+          controls
+          autoPlay
+          muted
+          loop
+          className="fixed bg-slate-900 w-[80vw] h-[50vh] lg:h-[70vh] z-[60] top-[calc(50vh-25vh)] lg:top-[15vh] left-[10vw] rounded-[3rem] shadow-xl shadow-cButtonShadowBlue"
+        >
+          <source src={videoSrc} type="video/mp4" />
+        </video>
+      )}
+
       <div
         className={`overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-screen h-screen bg-black md:inset-0 max-h-full opacity-55`}
-        onClick={() => setisOpen(false)}
+        onClick={() => {
+          setisOpen(false)
+          setSelectedVideo("")
+          }}
       ></div>
     </div>
   );
